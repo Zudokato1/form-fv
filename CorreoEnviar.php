8f19920e8aee825f9edef9dcb026ed9a76d48dcc
Zudokato <125523824+Zudokato1@users.noreply.github.com>
2023-03-09 20:53:08 -0500

    nuevo envio

--- CorreoEnviar.php
+++ CorreoEnviar.php
@@ -17,7 +17,19 @@ $email->SMTPAuth = true;
 $email->Port = 2525;
 $email->Username = 'c0ec0dfbe709d3';
 $email->Password = '152d9093641794';
+
+$email->setFrom('jdmosquerac2@academia.usbbog.edu.co');
+$email->addAddress('juandavidmc2020@gmail.com');
+
+$email->isHTML(true);
+$email->Subject ='contacto desde el formulario';
+$email->body    ='este es el contenido del mensaje <b>en negrita!</b>1';
+$email->AltBody ='Este es el contenido del mensaje en texto plano';
+
+$email->send();
+echo'El mensaje ha sido enviado';
+}catch(Exception $e) {
+    echo'el mensaje no se a podido enviar',$email->ErrorInfo;
 }
-$email->setFrom('jdmosquerac2@academia.usbbog.edu.co')
-$email->addAddress()
+
 ?>
